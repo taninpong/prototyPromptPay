43c252044e169e9a8a9f523132be4f5e75443a49
Sorry <so.sikarin@perfenterprise.com>
2022-05-15 16:34:34 +0700

    varidator

--- prototypes/src/app/transfer-to/transfer-to.page.ts
+++ prototypes/src/app/transfer-to/transfer-to.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-transfer-to',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TransferToPage implements OnInit {
 
-  constructor() { }
+  public fg: FormGroup;
+  public isFirstTime: boolean = true;
+
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.fg = this.fb.group({
+      'PPay': [null, [Validators.required, Validators.minLength(10), Validators.maxLength(15), Validators.pattern("^[0-9]+\.?([0-9]{1,2})?$")]],
+    });
+  }
 
   ngOnInit() {
   }
 
+  onSave() {
+    if (this.isFirstTime) {
+      this.isFirstTime = false;
+    }
+    if (this.fg.valid) {
+      console.log("1111");
+      this.router.navigate(['/ppay-payment-creating'])
+    }
+  }
+
+  isInvalid(name: string): boolean {
+    var ctrl = this.fg.get(name);
+    return ctrl.invalid && !this.isFirstTime;
+  }
 }
